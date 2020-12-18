7449ca8795154cee823a6512d65f5295cec99c37
shri4 <71806340+shri4@users.noreply.github.com>
2020-12-18 11:47:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<!DOCTYPE html> 
+
 <?php
-include_once("index.html");
+if(isset($_POST['submit'])){
+     $user = $_POST['username'];
+     $pass = $_POST['password'];
+     if($user=="admin" && $pass=="admin"){
+          echo("welcome");
+      }else {
+     echo("invalid");
+    }
+}
+
 ?>
+<html>
+
+<head>
+  <title>LOGIN</title>
+</head>
+<body>
+ <form action="#" method="get" accept-charset="utf-8">
+    <div id="login">
+      <strong>LOGIN </strong>
+    </div>
+    
+    <div id="inputtext">
+Name :<br />
+<input type="text" name="username" id="text" required placeholder="Your Name" />
+    </div>
+    
+    <div id="inputpassword">
+  
+      Password : <br />
+    
+      <input type="password" name="password" id="password" placeholder="Your Password" required/>
+    </div>
+    </div>
+
+    <div id="submitbtn">
+      <input type="submit" name="submit" id="submit" value="LOGIN" />
+    </div>
+  </form>
+</body>
+
+</html>
+
